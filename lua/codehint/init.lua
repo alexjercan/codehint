360c96b9f427667233ea7917c1db3753e5f139f9
Alex Jercan <jercan_alex27@yahoo.com>
2023-01-14 21:34:52 +0200

    fix: added linter and format

--- lua/codehint/init.lua
+++ lua/codehint/init.lua
@@ -1,4 +1,4 @@
-local json = require('cjson')
+local json = require("cjson")
 local cURL = require("cURL")
 
 local data_path = vim.fn.stdpath("data")
@@ -32,7 +32,7 @@ end
 
 local M = {}
 
-M.setup = function ()
+M.setup = function()
     local key = get_key()
 
     if not key then
@@ -47,9 +47,12 @@ M.hint = function()
     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     local line = vim.api.nvim_win_get_cursor(0)[1]
 
-    lines[line] = table.concat({lines[line], comment, "Fixme"}, " ")
+    lines[line] = table.concat({ lines[line], comment, "Fixme" }, " ")
 
-    local qstr = string.format("%s Q: Propose a hint that can help me fix the bug", comment)
+    local qstr = string.format(
+        "%s Q: Propose a hint that can help me fix the bug",
+        comment
+    )
     local astr = string.format("%s A:", comment)
     table.insert(lines, qstr)
     table.insert(lines, astr)
@@ -61,22 +64,26 @@ M.hint = function()
     }
 
     local body = string.format(
-       '{"model": "code-davinci-002", "prompt": %s, "max_tokens": 256, "temperature": 0.5, "top_p": 1}',
-       repr(prompt)
+        '{"model": "code-davinci-002", "prompt": %s, "max_tokens": 256, "temperature": 0.5, "top_p": 1}',
+        repr(prompt)
     )
 
     local url = "https://api.openai.com/v1/completions"
 
     cURL.easy({
-        url=url,
-        post=true,
-        httpheader=headers,
-        postfields=body,
-        writefunction=function(str)
+        url = url,
+        post = true,
+        httpheader = headers,
+        postfields = body,
+        writefunction = function(str)
             local tab = json.decode(str)
-            print(string.format("%s\n%s%s", qstr, astr, tab["choices"][1]["text"]))
-        end
-    }):perform():close()
+            print(
+                string.format("%s\n%s%s", qstr, astr, tab["choices"][1]["text"])
+            )
+        end,
+    })
+        :perform()
+        :close()
 end
 
 return M
